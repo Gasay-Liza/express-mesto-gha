4baeb699d165d413c44fca59f4006f87174d856f
Elizaveta Gasay <gasayliz@yandex.ru>
2023-04-23 19:42:29 +0300

    Добавлена обработка неправильного пути

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
   useNewUrlParser: true,
 });
 
+const { PORT = 3000 } = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use((req, res, next) => {
   req.user = {
@@ -29,8 +31,10 @@ module.exports.createCard = (req, res) => {
 app.use(userRouter);
 app.use(cardRouter);
 
-const { PORT = 3000 } = process.env;
-app.use(bodyParser.json());
+app.use('*', (req, res) => {
+  res.status(404).send({message :"ERROR 404"});
+});
+
 
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
